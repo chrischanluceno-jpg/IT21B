80e2effede2afec7a3dcebc0a44891ab3f27d8f7
chrischanluceno-jpg <chrischanluceno@gmail.com>
2025-09-05 08:59:31 +0800

    Added readline service and Initial readline codeblock

--- factorial.js
+++ factorial.js
@@ -1,5 +1,5 @@
 //Console Input
-const readline=require("redline");
+const readline=require("readline");
 
 const r1 = readline.createInterface({
     input:process.stdin,
@@ -14,14 +14,25 @@ console.log("3. Exit Application");
 console.log("--------------------------------");
 
 r1.question("Enter your choice (1-3): ", choice => {
-    if(choice == '1'){
+if (choice === "1"){
+
         console.log("Hello");
     } else if (choice === "2"){
         console.log("Factorial");
-    } else if (choice === "3"){
-        console.log("Exiting Application");
-    } else }{
-        console.log("Invalid Choice. Please try again");
-    }
+       
+        r1.question("Enter a number for factorial: ", numStr => {
+            let num = parseInt(numStr);
+             if (isNaN(num) || num < 0){
+                console.log("Please enter a valid non-negative integer.");
+            } else {
+                let factorial = 1;
 
-)
+                for (let i = 1; i <= num; i++){
+                    factorial *= i;
+                }
+                console.log("The factorial of num is factorial");
+            }
+        })
+    }
+})        
+    
