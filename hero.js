30d57fc007b5148e4a6641ac82d5fe2a3e56892a
chrischanluceno-jpg <chrischanluceno@gmail.com>
2025-09-16 08:15:14 +0800

    Added hero.js class instance and initial instance

---
+++ hero.js
@@ -0,0 +1,33 @@
+claass Hero{
+    #health
+
+    constructor(name,health.attack){
+    this.name = name;
+    this.#health = health;
+    this.attack = attack;
+  
+}
+
+    getName(){
+        return this.name;
+
+    }
+    get.Health(){
+        return this.#health;
+    }
+    showStats(){
+        console.log("Name:"+ this.name);
+        console.log("Heath:"+ this.#health);
+        console.log("Attack:"+ this.attack);
+        console.log("---------------");
+
+    }
+}
+
+
+const hero1 = new Hero("Thorin",100,10);
+
+const hero2 = new Hero("Gandalf",100,8);
+
+hero1.showStats();
+hero2.showStats();
